4616232aedc23b82d6ac8d0def986bfaa79ce521
李韦琼 <weiqiongli@sf-express.com>
2019-03-12 16:34:45 +0800

    modify classNames

--- classNames/classNames.py
+++ classNames/classNames.py
@@ -1,48 +1,51 @@
 import os
 
-#文件前缀
-filename_prefix = 'Youai'
-#文件后缀
-filename_suffix = 'Mustrong'
+# 文件前缀
+filename_prefix = 'SoulMate'
+# 文件后缀
+filename_suffix = 'Youbangtuwen'
 
-#需要更新文件目录
-target_path = '/Users/01378859/demo/LoveOfPomelo/LoveOfPomelo'
-#需要查询文件目录
-project_path = '/Users/01378859/demo/LoveOfPomelo/LoveOfPomelo/Sections'
-#工程文件地址
-pbxpro_path = '/Users/01378859/demo/LoveOfPomelo/LoveOfPomelo.xcodeproj/project.pbxproj'
+# 需要更新文件目录
+target_path = '/Users/xxxxxxxx/Downloads/PeachFriend/PeachFriend/Sections'
+# 需要查询文件目录
+project_path = '/Users/xxxxxxxx/Downloads/PeachFriend'
+# 工程文件地址
+pbxpro_path = '/Users/xxxxxxxx/Downloads/PeachFriend/PeachFriend.xcodeproj/project.pbxproj'
 
-#文件重命名函数，返回新的文件名
+
+# 文件重命名函数，返回新的文件名
 def file_rename(file_path):
     print(file_path)
-    root_path = os.path.split(file_path)[0]     # 文件目录
-    root_name = os.path.split(file_path)[1]     # 文件名包含扩展名
-    filename = os.path.splitext(root_name)[0]   # 文件名
-    filetype = os.path.splitext(root_name)[1]   # 文件扩展名
+    root_path = os.path.split(file_path)[0]  # 文件目录
+    root_name = os.path.split(file_path)[1]  # 文件名包含扩展名
+    filename = os.path.splitext(root_name)[0]  # 文件名
+    filetype = os.path.splitext(root_name)[1]  # 文件扩展名
     
     filename_new = filename
     if filename.startswith(filename_prefix) == False:
         filename_new = filename_prefix + filename_new
-    if filename.encode(filename_suffix) == False:
+    if filename.endswith(filename_suffix) == False:
         filename_new = filename_new + filename_suffix
     
     # 新文件名路径
-    new_path = os.path.join(root_path, filename_new + filetype)    # 拼接新路径
+    new_path = os.path.join(root_path, filename_new + filetype)  # 拼接新路径
     # 文件重命名
     os.renames(file_path, new_path)
     return filename_new
+
 
 def verify_filename(file_name):
     if file_name.endswith('.h') or file_name.endswith('.m') or file_name.endswith('.mm'):
         return True
     return False
 
-#定义一个字典 key=旧类名 value=新类名
+
+# 定义一个字典 key=旧类名 value=新类名
 needModifyDic = {}
 needModifyFileNameDict = {}
 
-#遍历文件，符合规则的进行重命名
-for (root, dirs, files) in os.walk(project_path):
+# 遍历文件，符合规则的进行重命名
+for (root, dirs, files) in os.walk(target_path):
     for file_name in files:
         if verify_filename(file_name):
             old_name = os.path.splitext(file_name)[0]
@@ -54,9 +57,10 @@ for (root, dirs, files) in os.walk(project_path):
             new_fileName = new_name + filetype
             needModifyFileNameDict[old_fileName] = new_fileName
 
-#遍历文件，在文件中更换新类名的引用
+
+# 遍历文件，在文件中更换新类名的引用
 print('------------------------------')
-for (root, dirs, files) in os.walk(target_path):
+for (root, dirs, files) in os.walk(project_path):
     for file_name in files:
         if verify_filename(file_name):
             print('-----folderName-------' + file_name)
@@ -72,7 +76,7 @@ for (root, dirs, files) in os.walk(target_path):
                             f4.write(s1)
                             f4.truncate()
                             f4.close()
-#替换配置文件中的类名
+# 替换配置文件中的类名
 print('==============================')
 for key in needModifyFileNameDict:
     with open(pbxpro_path, 'r+') as f:
@@ -86,3 +90,4 @@ for key in needModifyFileNameDict:
                 f2.write(s)
                 f2.truncate()
                 f2.close()
+
