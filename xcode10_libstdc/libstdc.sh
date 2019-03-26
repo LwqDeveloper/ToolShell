e0bb767ee274264e51216d48d20712c1251b540e
李韦琼 <weiqiongli@sf-express.com>
2019-03-26 16:46:59 +0800

    modify

--- xcode10_libstdc/libstdc.sh
+++ xcode10_libstdc/libstdc.sh
@@ -3,26 +3,7 @@ target_path_2="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platf
 target_path_3="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/"
 target_path_4="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/"
 
-cd 1
-sudo cp -r ./libstdc++.6.0.9.dylib ${target_path_1}
-sudo cp -r ./libstdc++.6.dylib ${target_path_1}
-sudo cp -r ./libstdc++.dylib ${target_path_1}
-cd ..
-
-cd 2
-sudo cp -r ./libstdc++.6.0.9.dylib ${target_path_2}
-sudo cp -r ./libstdc++.6.dylib ${target_path_2}
-sudo cp -r ./libstdc++.dylib ${target_path_2}
-cd ..
-
-cd 3
-sudo cp -r ./libstdc++.6.0.9.dylib ${target_path_3}
-sudo cp -r ./libstdc++.6.dylib ${target_path_3}
-sudo cp -r ./libstdc++.dylib ${target_path_3}
-cd ..
-
-cd 4
-sudo cp -r ./libstdc++.6.0.9.dylib ${target_path_4}
-sudo cp -r ./libstdc++.6.dylib ${target_path_4}
-sudo cp -r ./libstdc++.dylib ${target_path_4}
-cd ..
+sudo cp -r ./1 ${target_path_1}
+sudo cp -r ./2 ${target_path_2}
+sudo cp -r ./3 ${target_path_3}
+sudo cp -r ./4 ${target_path_4}
